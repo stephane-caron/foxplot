a33919e3c894c450a0a3d72f9612938a0cb6ccba
Stéphane Caron <stephane.caron@inria.fr>
2024-08-02 09:27:55 +0200

    fix: Allow input files with string values

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -11,6 +11,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 - Function: Absolute value
 
+### Fixed
+
+- Allow input files with string values
+
 ## [0.6.0] - 2024-06-17
 
 ### Added
--- foxplot/hot_series.py
+++ foxplot/hot_series.py
@@ -65,4 +65,5 @@ class HotSeries(Series):
         for index in range(max_index):
             last_value = self.__indexed_values.get(index, last_value)
             values.append(last_value)
-        return FrozenSeries(self._label, np.array(values, dtype=np.float64))
+        # Not setting the dtype below, as value type can be float, str, ...
+        return FrozenSeries(self._label, np.array(values))
