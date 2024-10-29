7d01b68e8df1d1f26f3caf2256c54af18569fdfb
Stéphane Caron <stephane.caron@inria.fr>
2024-10-29 16:55:20 +0100

    [minor] Apply ruff fix

--- foxplot/cli.py
+++ foxplot/cli.py
@@ -14,8 +14,8 @@ from typing import List, Union
 
 from .fox import Fox
 from .functions import abs as abs_func
-from .functions import std as std_func
 from .functions import estimate_lag
+from .functions import std as std_func
 from .node import Node
 from .series import Series
 
